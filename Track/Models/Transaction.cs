bee1c9540545bced3bdc7e9508bd6ef5f5c4fcd8
Qhorin <pjm5759@gmail.com>
2013-02-27 16:52:35 -0500

    Added transaction index view stuff

--- Track/Models/Transaction.cs
+++ Track/Models/Transaction.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -9,7 +10,9 @@ namespace Track.Models
     {
         public int TransactionId { get; set; }
         public DateTime Timestamp { get; set; }
+        public decimal TotalPrice { get; set; }
         
+        [DisplayName("Store")]
         public virtual Store StoreId { get; set; }
         public virtual ICollection<TransactionItem> TransactionItems { get; set; }
     }
